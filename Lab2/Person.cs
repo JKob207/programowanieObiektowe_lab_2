541b09983cea2a4573061c30390c7c07563b3837
Jakub.Kobialka <Jakub.Kobialka@PK5-08>
2022-03-19 11:34:54 +0100

    Dodaj pliki projektów.

---
+++ Lab2/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Person
+    {
+        protected string name { get; set; };
+        protected int age { get; set; };
+
+    }
+}
