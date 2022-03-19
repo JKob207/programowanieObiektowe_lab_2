fc99f589c42595b4aab0274fa312e5eaa047de9a
Jakub Kobiałka <Jakub.Kobialka@microsoft.wsei.edu.pl>
2022-03-19 11:57:00 +0100

    created Teacher class

---
+++ Lab2/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Teacher : Person
+    {
+        public Teacher(string n, int a) : base(n,a)
+        {
+            name = n;
+            age = a;
+        }
+        public override string ToString()
+        {
+            return $"Teacher: {name} ({age} y.o.)";
+        }
+    }
+}
