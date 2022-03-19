46e2fd38e9f597561a034b9f0b69547bfca8fdb7
Jakub Kobiałka <Jakub.Kobialka@microsoft.wsei.edu.pl>
2022-03-19 12:44:34 +0100

    created Classroom class

---
+++ Lab2/Classroom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Classroom
+    {
+        private string name { get; set; }
+        private Person[] persons { get; set; }
+
+        private Student _student;
+        private Teacher _teacher;
+
+        Classroom(string n, Person[] p)
+        {
+            name = n;
+            persons = p;
+        }
+
+        public override string ToString()
+        {
+            return $"Classroom: {name}";
+        }
+    }
+}
