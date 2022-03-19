e6398b1b3aaab6feacac03929e542c896fe4e0b1
Jakub Kobiałka <Jakub.Kobialka@microsoft.wsei.edu.pl>
2022-03-19 11:47:30 +0100

    created Person class

--- Lab2/Person.cs
+++ Lab2/Person.cs
@@ -8,8 +8,23 @@ namespace Lab2
 {
     class Person
     {
-        protected string name { get; set; };
-        protected int age { get; set; };
+        protected string name { get; set; }
+        protected int age { get; set; }
 
+        public Person (string n, int a)
+        {
+            name = n;
+            age = a;
+        }
+
+        public bool Equals(Person p)
+        {
+            return this.name.Equals(p.name) && this.age.Equals(p.name);
+        }
+
+        public override string ToString()
+        {
+            return $"{name} ({age} y.o.)";
+        }
     }
 }
