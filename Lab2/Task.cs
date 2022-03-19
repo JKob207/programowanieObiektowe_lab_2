8fca1d113ee44ae62f8e431e600bb50a3483c6c6
Jakub Kobiałka <Jakub.Kobialka@microsoft.wsei.edu.pl>
2022-03-19 12:12:53 +0100

    created Task class

---
+++ Lab2/Task.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    enum TaskStatus
+    {
+        Waiting,
+        Progress,
+        Done,
+        Rejected
+    }
+
+    class Task
+    {
+        private string name;
+        private TaskStatus status;
+
+        public Task(string n, TaskStatus s)
+        {
+            name = n;
+            status = s;
+        }
+        public override string ToString()
+        {
+            return $"{name} [{status}]";
+        }
+        public bool Equals(Task t)
+        {
+            if (t == null) return false;
+            if (t == this) return true;
+
+            return Object.Equals(this.name, t.name) && Object.Equals(this.status, t.status);
+        }
+    }
+}
